1be696a5508706ee394eb2358544b239538e70d6
Dave Boden <dave.boden@app-iron.net>
2016-12-04 06:04:27 +0100

    v0.2.4 added timeout setting

--- src/Cake.NSpec/NSpecBinder.cs
+++ src/Cake.NSpec/NSpecBinder.cs
@@ -13,68 +13,70 @@ using Cake.Common.Diagnostics;
 
 namespace Cake.NSpec
 {
-	[CakeAliasCategory("Testing")]
-	[CakeAliasCategory("BDD")]
-	[CakeAliasCategory("NSpec")]
+	[CakeAliasCategory ("Testing")]
+	[CakeAliasCategory ("BDD")]
+	[CakeAliasCategory ("NSpec")]
 	public static class NSpecBinder
 	{
+
 		[CakeMethodAlias]
-		public static void NSpec(this ICakeContext context, string pattern)
+		public static void NSpec (this ICakeContext context, string pattern)
 		{
-			if (context == null)
-			{
-				throw new ArgumentNullException(nameof(context));
+			NSpec (context, pattern, -1);
+		}
+
+		[CakeMethodAlias]
+		public static void NSpec (this ICakeContext context, string pattern, int timeout)
+		{
+			if (context == null) {
+				throw new ArgumentNullException (nameof (context));
 			}
 
-			var assemblies = context.Globber.GetFiles(pattern).ToArray();
-			var isOnMono = Type.GetType("Mono.Runtime") != null;
+			var assemblies = context.Globber.GetFiles (pattern).ToArray ();
+			var isOnMono = Type.GetType ("Mono.Runtime") != null;
 
-			var cakeNSpec = System.IO.Path.GetDirectoryName(
-				new System.Uri(typeof(NSpecBinder).Assembly.CodeBase).LocalPath
+			var cakeNSpec = System.IO.Path.GetDirectoryName (
+				new System.Uri (typeof (NSpecBinder).Assembly.CodeBase).LocalPath
 			);
-			context.Debug($"nspec: setting working directory {cakeNSpec}");
-			if (assemblies.Length == 0)
-			{
-				context.Log.Warning($"nspec: The provided pattern did not match any files. ({pattern})");
+			context.Debug ($"nspec: setting working directory {cakeNSpec}");
+			if (assemblies.Length == 0) {
+				context.Log.Warning ($"nspec: The provided pattern did not match any files. ({pattern})");
 				return;
-			}
-			else
-			{
-				var proc = typeof(Runner).Assembly.GetName().Name + ".exe";
+			} else {
+				var proc = typeof (Runner).Assembly.GetName ().Name + ".exe";
 
 				var procArgs = "";
 
-				if (isOnMono)
-				{
+				if (isOnMono) {
 					procArgs = proc + " ";
 					proc = "mono";
 				}
 
-				foreach (var asm in assemblies)
-				{
-					var fileName = asm.GetFilename();
-					var localProcArgs = procArgs + System.IO.Path.Combine( asm.GetDirectory().FullPath , fileName.FullPath);
+				foreach (var asm in assemblies) {
+					var fileName = asm.GetFilename ();
+					var localProcArgs = procArgs + System.IO.Path.Combine (asm.GetDirectory ().FullPath, fileName.FullPath);
 					localProcArgs += " -f console";
 
-					context.Log.Information($"Testing Spec {fileName.FullPath}");
-					context.Log.Debug($"nspec: Found Assembly {asm.GetDirectory().FullPath}/{fileName.FullPath}");
-					context.Log.Debug($"nspec: Starting {proc} {localProcArgs}");
+					context.Log.Information ($"Testing Spec {fileName.FullPath}");
+					context.Log.Debug ($"nspec: Found Assembly {asm.GetDirectory ().FullPath}/{fileName.FullPath}");
+					context.Log.Debug ($"nspec: Starting {proc} {localProcArgs}");
 
-					var proccess = context.ProcessRunner.Start(proc, new ProcessSettings
-					{
-						Arguments = ProcessArgumentBuilder.FromString(localProcArgs),
+					var proccess = context.ProcessRunner.Start (proc, new ProcessSettings {
+						Arguments = ProcessArgumentBuilder.FromString (localProcArgs),
 						WorkingDirectory = cakeNSpec
 					});
 
-					proccess.WaitForExit(30000);
-					var errorCode = proccess.GetExitCode();
-					if (errorCode != 0)
-					{
-						throw new CakeException($"Runner {fileName} exited with {errorCode}");
+					if (timeout > 0) {
+						proccess.WaitForExit ((int)timeout);
+					} else {
+						proccess.WaitForExit ();
 					}
-					else
-					{
-						context.Log.Debug($"{fileName} passed.");
+
+					var errorCode = proccess.GetExitCode ();
+					if (errorCode != 0) {
+						throw new CakeException ($"Runner {fileName} exited with {errorCode}");
+					} else {
+						context.Log.Debug ($"{fileName} passed.");
 					}
 				}
 			}
